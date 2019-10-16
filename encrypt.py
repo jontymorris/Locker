14db27e09c54ab1084bce4a3f15327dfa3c27945
Jonty Morris <jonty.morris@yahoo.co.nz>
2019-10-17 08:14:38 +1300

    Split up code

---
+++ encrypt.py
@@ -0,0 +1,113 @@
+import vigenere
+import system
+import os
+
+
+def should_process_file(path):
+    '''
+    Returns whether the given file should be processed
+    based on if the file's extension is in a given whitelist.
+    '''
+
+    whitelist = [
+        '',
+        '.txt',
+        '.py',
+        '.js',
+    ]
+
+    extension = os.path.splitext(path)[1]
+
+    return extension.lower() in whitelist
+
+
+def read_file(path):
+    '''
+    Reads the given file and returns it's contents.
+    '''
+
+    handle = open(path, 'r', encoding='utf-8')
+    contents = handle.read()
+    handle.close()
+
+    return contents
+
+
+def write_file(path, contents):
+    '''
+    Writes the contents to the given file.
+    '''
+
+    handle = open(path, 'w', encoding='utf-8')
+    handle.write(contents)
+    handle.close()
+
+
+def is_folder(path):
+    '''
+    Checks if the given path is a folder.
+    '''
+
+    return os.path.isdir(path)
+
+
+def encrypt_file(path, password):
+    '''
+    Encrypts the given the file with the password.
+    '''
+
+    if not should_process_file(path):
+        return
+
+    try:
+        contents = read_file(path)
+        contents = vigenere.encrypt(contents, password)
+        write_file(path, contents)
+    except:
+        print('[Error] Failed to encrypt {}'.format(path))
+
+
+def decrypt_file(path, password):
+    '''
+    Decrypts the given the file with the password.
+    '''
+
+    if not should_process_file(path):
+        return
+
+    try:
+        contents = read_file(path)
+        contents = vigenere.decrypt(contents, password)
+        write_file(path, contents)
+    except:
+        print('[Error] Failed to decrypt {}'.format(path))
+
+
+def encrypt_folder(path, password):
+    '''
+    Recursively encrypts the given folder with the password.
+    '''
+
+    for name in os.listdir(path):
+        current_path = os.path.join(path, name)
+
+        if is_folder(current_path):
+            encrypt_folder(current_path, password)
+
+        else:
+            encrypt_file(current_path, password)
+
+
+def decrypt_folder(path, password):
+    '''
+    Recursively decrypts the given folder with the password.
+    '''
+
+    for name in os.listdir(path):
+        current_path = os.path.join(path, name)
+
+        if is_folder(current_path):
+            decrypt_file(current_path, password)
+
+        else:
+            decrypt_file(current_path, password)
--- locker.py
+++ locker.py
@@ -1,117 +1,7 @@
 import sys
 import os
 import getpass
-import vigenere
-
-
-def read_file(path):
-    '''
-    Reads the given file and returns it's contents.
-    '''
-
-    handle = open(path, 'r', encoding='utf-8')
-    contents = handle.read()
-    handle.close()
-
-    return contents
-
-
-def write_file(path, contents):
-    '''
-    Writes the contents to the given file.
-    '''
-
-    handle = open(path, 'w', encoding='utf-8')
-    handle.write(contents)
-    handle.close()
-
-
-def should_process_file(path):
-    '''
-    Returns whether the given file should be processed
-    based on if the file's extension is in a given whitelist.
-    '''
-
-    whitelist = [
-        '',
-        '.txt',
-        '.py',
-        '.js',
-    ]
-
-    extension = os.path.splitext(path)[1]
-
-    return extension.lower() in whitelist
-
-
-def encrypt_file(path, password):
-    '''
-    Encrypts the given the file with the password.
-    '''
-
-    if not should_process_file(path):
-        return
-
-    try:
-        contents = read_file(path)
-        contents = vigenere.encrypt(contents, password)
-        write_file(path, contents)
-    except:
-        print('[Error] Failed to encrypt {}'.format(path))
-
-
-def decrypt_file(path, password):
-    '''
-    Decrypts the given the file with the password.
-    '''
-
-    if not should_process_file(path):
-        return
-
-    try:
-        contents = read_file(path)
-        contents = vigenere.decrypt(contents, password)
-        write_file(path, contents)
-    except:
-        print('[Error] Failed to decrypt {}'.format(path))
-
-
-def encrypt_folder(path, password):
-    '''
-    Recursively encrypts the given folder with the password.
-    '''
-
-    for name in os.listdir(path):
-        current_path = os.path.join(path, name)
-
-        if is_folder(current_path):
-            encrypt_folder(current_path, password)
-
-        else:
-            encrypt_file(current_path, password)
-
-
-def decrypt_folder(path, password):
-    '''
-    Recursively decrypts the given folder with the password.
-    '''
-
-    for name in os.listdir(path):
-        current_path = os.path.join(path, name)
-
-        if is_folder(current_path):
-            decrypt_file(current_path, password)
-
-        else:
-            decrypt_file(current_path, password)
-
-
-def is_folder(path):
-    '''
-    Checks if the given path is a folder.
-    '''
-
-    return os.path.isdir(path)
+import encrypt
 
 
 def show_usage():
@@ -150,20 +40,20 @@ if __name__ == '__main__':
     # encrypt
     if option == 'encrypt':
         # folder
-        if is_folder(path):
-            encrypt_folder(path, password)
+        if encrypt.is_folder(path):
+            encrypt.encrypt_folder(path, password)
         # file
         else:
-            encrypt_file(path, password)
+            encrypt.encrypt_file(path, password)
 
     # decrypt
     elif option == 'decrypt':
         # folder
-        if is_folder(path):
-            decrypt_folder(path, password)
+        if encrypt.is_folder(path):
+            encrypt.decrypt_folder(path, password)
         # file
         else:
-            decrypt_file(path, password)
+            encrypt.decrypt_file(path, password)
 
     # invalid
     else:
